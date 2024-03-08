33d3f24d025600a3d94bbcab730e9e6eafb0619e
Kiran_Kutte <kirankutte28@gmail.com>
2024-03-08 12:26:59 +0530

    Introduced UUID for Basemodel class and updated UUID from ID

--- src/main/java/com/productservice/productservice/models/BaseModel.java
+++ src/main/java/com/productservice/productservice/models/BaseModel.java
@@ -1,15 +1,24 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.GenericGenerators;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseModel{
     @Id
-    private long id;
+    @GeneratedValue(generator= "uuidgenerator")
+    @GenericGenerator(name="uuidgenerator",strategy = "uuid2")
+    @Column(name = "id",columnDefinition = "binary(16)",nullable = false,updatable = false)
+    private UUID id;
 
 }
